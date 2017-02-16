4f1326f58c6ee99db735b35b4663d4a7ddc5d70e
Janek Brandt and Stormi Hoebelheinrich <janek@>
2017-02-16 11:05:56 -0800

    add js to show results based on user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 $(function(){
+  $("#formOne").submit(function() {
+    event.preventDefault();
+    var input1 = $("input:radio[name=question1]:checked").val();
+    var input2 = $("input:radio[name=question2]:checked").val();
+    var input3 = $("input:radio[name=question3]:checked").val();
 
+    $(".quizResult p").hide();
+    
+    if (input1 === "yes" && input2 === "obama" && input3 === "yes") {
+      $("#liberal").show();
+    } else if (input1 === "no" && input2 === "reagan" && input3 === "no") {
+      $("#conservative").show();
+    } else {
+      $("#moderate").show();
+    };
+
+  });
 
 });
